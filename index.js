16d565f1c8a3ef14cb898a54cac42c77cfd28e71
GHSoyad <ghsoyad@gmail.com>
2022-11-06 15:31:18 +0600

    Added database, products collection and path

--- index.js
+++ index.js
@@ -2,11 +2,35 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
 app.use(cors())
 app.use(express.json())
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7r6jn89.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+        const productsCollection = client.db('ema-john').collection('products');
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+    } finally {
+
+    }
+}
+
+run().catch(error => console.log(error))
 
 app.get('/', (req, res) => {
     res.send("Server is running!!")
