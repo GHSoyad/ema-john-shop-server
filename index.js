c948932402602439994a5f2a6143a432dda811b7
GHSoyad <ghsoyad@gmail.com>
2022-11-06 22:29:11 +0600

    Added orders collection and query

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId, ObjectID } = require('mongodb');
 
 // middleware
 app.use(cors())
@@ -17,11 +17,45 @@ async function run() {
 
     try {
         const productsCollection = client.db('ema-john').collection('products');
+        const ordersCollection = client.db('ema-john').collection('orders');
 
         app.get('/products', async (req, res) => {
             const query = {};
             const cursor = productsCollection.find(query);
             const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const cursor = productsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/orders', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = ordersCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.put('/orders', async (req, res) => {
+            const product = req.body;
+            const { productId, email } = product;
+            const filter = { _id: ObjectId(productId) };
+            const options = { upsert: true };
+            const updateOrder = {
+                $set: { productId, email },
+                $inc: { quantity: 1 }
+            };
+            const result = await ordersCollection.updateOne(filter, updateOrder, options);
             res.send(result);
         })
 
